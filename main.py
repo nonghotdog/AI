0335fd18779616e400fd59879b01cc55529b8357
nonghotdog <puwadat3215@gmail.com>
2025-03-15 23:11:48 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+import streamlit as st
+import component as cp10519
+
+st.set_page_config(page_title="6604062610519 Intelligent System Project", page_icon=":computer:", layout="wide")
+cp10519.SideBar()
