415b4c467b8a8dbf45de01cc004f3fa3bcb5ecaa
nonghotdog <puwadat3215@gmail.com>
2025-03-16 01:19:05 +0700

    [del] component.py v2

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import streamlit as st
-import component as cp10519
 
 st.set_page_config(page_title="6604062610519 Intelligent System Project", page_icon=":computer:", layout="wide")
-cp10519.SideBar()
 
 def SideBar():
     st.navigation(tabs).run()
